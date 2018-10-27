1e6c6a097b895c57ef6fabbc2a3c383c9c21d2e6
Oops <16302010059@fudan.edu.cn>
2018-10-27 15:10:45 +0800

    bug。。忘记break。。

--- src/MainApp.java
+++ src/MainApp.java
@@ -14,7 +14,7 @@ public class MainApp {
         PreRead preRead = (PreRead) context.getBean("preRead");
         preRead.create();
 
-        System.out.println("1：csv\n2：sqlite\n3: csv & sqlite\n选择数据源（输入数字）：");
+        System.out.print("\n1：csv\n2：sqlite\n3: csv & sqlite\n选择数据源（输入数字）：");
 
         Scanner scanner = new Scanner(System.in);
         String numStr = scanner.next();
@@ -26,6 +26,7 @@ public class MainApp {
             case "2":
             case "sqlite": //从sqlite数据源导入数据
                 ((ImportServiceDao) context.getBean("serviceSqlite")).readData();
+                break;
             case "3":
             case "csv & sqlite":
                 ((ImportServiceDao) context.getBean("serviceCsv")).readData();
