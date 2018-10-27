9a745606b1e7bf997476f20b3d5118f02498c087
Oops <16302010059@fudan.edu.cn>
2018-10-27 14:13:00 +0800

    切面，打印读取数据库的日志

---
+++ src/logger/LoggerAspect.java
@@ -0,0 +1,22 @@
+package logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggerAspect {
+
+    @Around("execution(void dao.ImportServiceDao.readData())")
+    public Object log(ProceedingJoinPoint point) throws Throwable {
+        long time1 = System.currentTimeMillis();
+        Object obj = point.proceed();
+        System.out.println("耗时：" + (System.currentTimeMillis() - time1) + "ms");
+        System.out.println("共读入" + Logger.getInsertCount() + "条数据！");
+        System.out.println(Logger.getDupCount() + "条重复数据读入失败！");
+        Logger.reset();
+        return obj;
+    }
+}
